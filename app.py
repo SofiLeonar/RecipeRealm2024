3ff1564950b751bbcc90c59403dc1f731ea86406
SofiLeonar <143642462+SofiLeonar@users.noreply.github.com>
2024-10-15 15:47:40 -0300

    Feat: Rutas nav

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, url_for, request, redirect
-import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/nosotros')
+def nosotros():
+    return render_template('nosotros.html')
+
+@app.route('/compras')
+def compras():
+    return render_template('compras.html')
+
+@app.route('/preguntas')
+def preguntas():
+    return render_template('preguntasFrecuentes.html')
 
 if __name__ =='__main__':
     app.run(debug=True)
