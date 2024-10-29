234a9e57dd0cf2cd2179ee4b8b0a8aac40e6026e
Constanza Delfino <111506427+cdelfino@users.noreply.github.com>
2024-10-28 21:33:32 -0300

    create config

---
+++ config.py
@@ -0,0 +1,12 @@
+# Configuración usuarios
+JSONBIN_USERS_URL = 'https://api.jsonbin.io/v3/b/67056a8fad19ca34f8b50970'
+HEADERS_USERS = {
+    "Content-Type": 'application/json',
+    "X-Master-Key": '$2a$10$1UFHJ7B89yDmWCd/HBP5xO1idjuzb0siHyQ2QNroWFWeO74FLn5Fi'
+}
+
+# Configuración cursos
+JSONBIN_CURSOS_URL = 'https://api.jsonbin.io/v3/b/671fd915e41b4d34e44a5268'
+HEADERS_CURSOS = {
+    'X-Master-Key': '$2a$10$5JBDXm2EeTkVYtrUY/NQwOPPwqJ9/G.CCVNHkwYCtM4Yna4/6jUye'
+}
