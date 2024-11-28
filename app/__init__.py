c69629bd909aa2ce390675d8ed99573bdcb4d56e
Sosa-Nazarena <mariana.sosa@davinci.edu.ar>
2024-11-27 22:59:51 -0300

    feat: prueba 2 para render

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from app.blueprints.auth.routes import auth_bp
 from app.blueprints.dashboard.routes import dashboard_bp
+from flask_mysqldb import MySQL
+import os
+from dotenv import load_dotenv
+
+
+mysql = MySQL()
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'vacac'
-    
+    load_dotenv()
+
+    app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+    app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+    app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+
+    mysql.init_app(app)
+
+
+    #app.config['SECRET_KEY'] = 'vacac'
+
     app.register_blueprint(auth_bp)
     app.register_blueprint(dashboard_bp)
-    
+
     @app.route('/')
     def index():
         return render_template('index.html')
